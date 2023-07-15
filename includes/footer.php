4984bc78ff0762aeab006e8c797475a61c2d0060
CurvaturaMajor <62203446+curvaturamajor@users.noreply.github.com>
2023-07-16 02:18:36 +0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<div class="row">
+                            <div class="col-md-12">
+                                <div class="copyright">
+                                    <p>Misafir Kayıt Sistemi 2022. İhlas Holding A.Ş.</p>
+                                </div>
+                            </div>
+                        </div>
